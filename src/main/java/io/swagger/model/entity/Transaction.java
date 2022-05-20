7c5fab1eec6034fca5e0dbddd9f34ac1ba0448ee
GEZUS95 <ceesgribnau@hotmail.com>
2022-05-20 15:01:21 +0200

    added Transaction entity

--- src/main/java/io/swagger/model/entity/Transaction.java
+++ src/main/java/io/swagger/model/entity/Transaction.java
@@ -1,4 +1,75 @@
 package io.swagger.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
 public class Transaction {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String ibanFrom;
+    private String ibanTo;
+    private Number amount;
+    private Integer issuedBy;
+    private Integer iat;
+
+    public Transaction(Integer id, String ibanFrom, String ibanTo, Number amount, Integer issuedBy, Integer iat) {
+        this.id = id;
+        this.ibanFrom = ibanFrom;
+        this.ibanTo = ibanTo;
+        this.amount = amount;
+        this.issuedBy = issuedBy;
+        this.iat = iat;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIbanFrom() {
+        return ibanFrom;
+    }
+
+    public void setIbanFrom(String ibanFrom) {
+        this.ibanFrom = ibanFrom;
+    }
+
+    public String getIbanTo() {
+        return ibanTo;
+    }
+
+    public void setIbanTo(String ibanTo) {
+        this.ibanTo = ibanTo;
+    }
+
+    public Number getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Number amount) {
+        this.amount = amount;
+    }
+
+    public Integer getIssuedBy() {
+        return issuedBy;
+    }
+
+    public void setIssuedBy(Integer issuedBy) {
+        this.issuedBy = issuedBy;
+    }
+
+    public Integer getIat() {
+        return iat;
+    }
+
+    public void setIat(Integer iat) {
+        this.iat = iat;
+    }
 }
