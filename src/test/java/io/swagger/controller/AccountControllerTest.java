3470347d5d7a2353b24d0618304fd5840c0b4488
marbwoulret <bramterlouw12@gmail.com>
2022-05-31 21:39:30 +0200

    Helemaal klaar met die klote tests

--- src/test/java/io/swagger/controller/AccountControllerTest.java
+++ src/test/java/io/swagger/controller/AccountControllerTest.java
@@ -1,11 +1,10 @@
 package io.swagger.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.api.AccountsApiController;
 import io.swagger.model.entity.Account;
 import io.swagger.model.enumeration.AccountType;
 import io.swagger.service.AccountService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,7 +13,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
@@ -33,12 +32,12 @@ public class AccountControllerTest {
     @MockBean
     private AccountService accountService;
 
-    @Autowired
-    private ObjectMapper mapper;
+//    @Autowired
+//    private ObjectMapper mapper;
 
     @Test
     public void getAllShouldReturnJsonArrayOfSizeOne() throws Exception {
-        when(accountService.getAll(0, 10)).thenReturn(Arrays.asList(new Account("NL01INHO0000000002", AccountType.CURRENT, "1234", 2, new BigDecimal(20), new BigDecimal(20), true)));
+        when(accountService.getAll(0, 10)).thenReturn(List.of(new Account("NL01INHO0000000002", AccountType.CURRENT, "1234", 2, new BigDecimal(20), new BigDecimal(20), true)));
         this.mockMvc.perform(get("/accounts"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
