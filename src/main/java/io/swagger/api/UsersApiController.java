a190f1fd74c464a4e94140b868201ceef6fefc28
Bastiaan van der Bijl <666669@student.inholland.nl>
2022-05-31 21:14:34 +0200

    Get all users + has account parameter

--- src/main/java/io/swagger/api/UsersApiController.java
+++ src/main/java/io/swagger/api/UsersApiController.java
@@ -9,6 +9,7 @@ import io.swagger.model.UsersLoginBody;
 import io.swagger.model.entity.User;
 import io.swagger.model.enumeration.Role;
 import io.swagger.security.JwtTokenProvider;
+import io.swagger.service.AccountService;
 import io.swagger.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -45,6 +46,9 @@ public class UsersApiController implements UsersApi {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AccountService accountService;
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
@@ -91,7 +95,7 @@ public class UsersApiController implements UsersApi {
     }
 
     //    @PreAuthorize("hasRole('EMPLOYEE')")
-    public ResponseEntity<List<UserResponseDTO>> getAllUsers(@Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "offset", required = false) Integer offset, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "limit", required = false) Integer limit, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "firstname", required = false) String firstname, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "lastname", required = false) String lastname, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "activated", required = false) Boolean activated) {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers(@Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "offset", required = false) Integer offset, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "limit", required = false) Integer limit, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "firstname", required = false) String firstname, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "lastname", required = false) String lastname, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "activated", required = false) Boolean activated, @Parameter(in = ParameterIn.QUERY, description = "", schema = @Schema()) @Valid @RequestParam(value = "hasaccount", required = false) Boolean hasAccount) {
 
         if (offset == null) {
             offset = 0;
@@ -106,6 +110,7 @@ public class UsersApiController implements UsersApi {
         ModelMapper modelMapper = new ModelMapper();
         List<User> usersFilteredNames = new ArrayList<>();
         List<User> usersFilteredStatus = new ArrayList<>();
+        List<User> userFilteredHasAccount = new ArrayList<>();
 
         // filters users on firstname, lastname or both
         for (User s : users) {
@@ -153,8 +158,25 @@ public class UsersApiController implements UsersApi {
             usersFilteredStatus = usersFilteredNames;
         }
 
+        // filters users on owning a account or not
+        if (hasAccount != null) {
+            for (User s : usersFilteredStatus) {
+                if (hasAccount) {
+                    if (accountService.getAllByUserId(s.getId()) != null) {
+                        userFilteredHasAccount.add(s);
+                    }
+                } else {
+                    if (accountService.getAllByUserId(s.getId()) == null) {
+                        userFilteredHasAccount.add(s);
+                    }
+                }
+            }
+        } else {
+            userFilteredHasAccount = usersFilteredStatus;
+        }
+
         // use mapper to map all users to user response data transfer object
-        List<UserResponseDTO> responseDTOS = usersFilteredStatus.stream().map(user -> modelMapper.map(user, UserResponseDTO.class)).collect(Collectors.toList());
+        List<UserResponseDTO> responseDTOS = userFilteredHasAccount.stream().map(user -> modelMapper.map(user, UserResponseDTO.class)).collect(Collectors.toList());
 
 
         // return all response dto's and http 200
