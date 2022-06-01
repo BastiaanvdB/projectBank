6ea766d2de67e3643589962abfb15ea589b2abe1
marbwoulret <bramterlouw12@gmail.com>
2022-06-01 15:28:32 +0200

    per ongeluk properties verwijderd

--- src/test/java/io/swagger/controller/AccountControllerTest.java
+++ src/test/java/io/swagger/controller/AccountControllerTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.test.context.support.WithMockUser;
