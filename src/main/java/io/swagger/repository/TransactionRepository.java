7c390b458c681cd081f0d7d935d06760db4f6315
GEZUS95 <ceesgribnau@hotmail.com>
2022-06-01 13:12:55 +0200

    removed unused imports

--- src/main/java/io/swagger/repository/TransactionRepository.java
+++ src/main/java/io/swagger/repository/TransactionRepository.java
@@ -1,21 +1,11 @@
 package io.swagger.repository;
 
 import io.swagger.model.entity.Transaction;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.metamodel.Metamodel;
 import java.util.List;
-import java.util.Map;
-
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String>, TransactionRepositoryCustom {
