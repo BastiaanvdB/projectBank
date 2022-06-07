4653bcf7c82cb5abe694faaaea461d4804d6e61b
Bastiaan van der Bijl <666669@student.inholland.nl>
2022-06-07 23:42:07 +0200

    swagger override

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -14,6 +14,7 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-17T11:45:05.257Z[GMT]")
@@ -35,7 +36,10 @@ public class SwaggerDocumentationConfig {
 
     @Bean
     public Docket customImplementation() {
+        Server serverLocal = new Server("Test", "http://localhost:8080", "Testing server", Collections.emptyList(), Collections.emptyList());
+        Server productionServer = new Server("Production", "https://api.bbcbank.nl", "Production server", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
+                .servers(productionServer, serverLocal)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
                 .build()
@@ -54,11 +58,11 @@ public class SwaggerDocumentationConfig {
         return new ApiInfoBuilder()
                 .title("Bank-api")
                 .description("Bank api van project groep 1")
-                .license("")
-                .licenseUrl("http://unlicense.org")
-                .termsOfServiceUrl("")
+                .license("\"Life, Uh, Finds a Way” - Dr. Ian Malcolm")
+                .licenseUrl("https://www.bbcbank.nl")
+                .termsOfServiceUrl("https://www.bbcbank.nl")
                 .version("1.0.0")
-                .contact(new Contact("", "", ""))
+                .contact(new Contact("Bastiaan, Bram, Cees", "https://www.bbcbank.nl", "666669@student.inholland.nl"))
                 .build();
     }
 
