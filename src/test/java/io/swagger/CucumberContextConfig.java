a73c0d5db8fb65893b91715aaf58acf6f9314d3e
marbwoulret <bramterlouw12@gmail.com>
2022-06-14 21:59:24 +0200

    tests run

--- src/test/java/io/swagger/CucumberContextConfig.java
+++
@@ -1,9 +0,0 @@
-package io.swagger;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.test.context.ContextConfiguration;
-
-@CucumberContextConfiguration
-@ContextConfiguration
-public class CucumberContextConfig {
-}
