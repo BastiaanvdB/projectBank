2f5ed0633e13b659a83f1f9980bfcb4c8a429005
Bastiaan van der Bijl <666669@student.inholland.nl>
2022-06-21 22:55:44 +0200

    t

--- src/main/java/io/swagger/model/DTO/UserFilterDTO.java
+++ src/main/java/io/swagger/model/DTO/UserFilterDTO.java
@@ -2,11 +2,12 @@ package io.swagger.model.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserFilterDTO {
 
