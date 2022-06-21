76bc481324d47ab93e607e8868bc6330526003e8
marbwoulret <bramterlouw12@gmail.com>
2022-06-21 20:02:07 +0200

    user cucumber

---
+++ src/test/java/io/swagger/steps/user/UserStepDefinitions.java
@@ -0,0 +1,148 @@
+package io.swagger.steps.user;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserStepDefinitions {
+    @Given("I have valid jwt to create new user")
+    public void iHaveValidJwtToCreateNewUser() {
+    }
+
+    @When("I call endpoint to create new user")
+    public void iCallEndpointToCreateNewUser() {
+    }
+
+    @Then("I receive http code {int} created new user")
+    public void iReceiveHttpCodeCreatedNewUser(int status) {
+    }
+
+    @And("I get newly created user")
+    public void iGetNewlyCreatedUser() {
+    }
+
+
+
+    @Given("I have valid jwt to get all users")
+    public void iHaveValidJwtToGetAllUsers() {
+    }
+
+    @When("I call endpoint to get all users")
+    public void iCallEndpointToGetAllUsers() {
+    }
+
+    @Then("I receive http code {int} for get all users")
+    public void iReceiveHttpCodeForGetAllUsers(int status) {
+    }
+
+    @And("I get list with all users")
+    public void iGetListWithAllUsers() {
+    }
+
+
+
+    @Given("I have valid jwt to get one user")
+    public void iHaveValidJwtToGetOneUser() {
+    }
+
+    @When("I call endpoint to get one user")
+    public void iCallEndpointToGetOneUser() {
+    }
+
+    @Then("I receive http code {int} for get one user")
+    public void iReceiveHttpCodeForGetOneUser(int status) {
+    }
+
+    @And("I get one user")
+    public void iGetOneUser() {
+    }
+
+
+
+    @Given("I have valid jwt to set the password of user")
+    public void iHaveValidJwtToSetThePasswordOfUser() {
+    }
+
+    @When("I call endpoint to set password of user")
+    public void iCallEndpointToSetPasswordOfUser() {
+    }
+
+    @Then("I receive http code {int} for setting password")
+    public void iReceiveHttpCodeForSettingPassword(int status) {
+    }
+
+
+
+    @Given("I have valid jwt to set the role of user")
+    public void iHaveValidJwtToSetTheRoleOfUser() {
+    }
+
+    @When("I call endpoint to set role of user")
+    public void iCallEndpointToSetRoleOfUser() {
+    }
+
+    @Then("I receive http code {int} for setting role")
+    public void iReceiveHttpCodeForSettingRole(int status) {
+    }
+
+
+
+    @Given("I have valid jwt to set the status of user")
+    public void iHaveValidJwtToSetTheStatusOfUser() {
+    }
+
+    @When("I call endpoint to set status of user")
+    public void iCallEndpointToSetStatusOfUser() {
+    }
+
+    @Then("I receive http code {int} for setting status")
+    public void iReceiveHttpCodeForSettingStatus(int status) {
+    }
+
+
+
+    @Given("I have valid jwt to update user")
+    public void iHaveValidJwtToUpdateUser() {
+    }
+
+    @When("I call endpoint to update user")
+    public void iCallEndpointToUpdateUser() {
+    }
+
+    @Then("I receive http code {int} for updating user")
+    public void iReceiveHttpCodeForUpdatingUser(int status) {
+    }
+
+
+
+    @Given("I have valid credentials to log in")
+    public void iHaveValidCredentialsToLogIn() {
+    }
+
+    @When("I call endpoint to log into the system")
+    public void iCallEndpointToLogIntoTheSystem() {
+    }
+
+    @Then("I receive http code {int} for loggin into the system")
+    public void iReceiveHttpCodeForLogginIntoTheSystem(int status) {
+    }
+
+    @And("I receive valid jwt token")
+    public void iReceiveValidJwtToken() {
+    }
+
+
+
+    @Given("I have valid jwt to get current user")
+    public void iHaveValidJwtToGetCurrentUser() {
+    }
+
+    @When("I call endpoint to get current user")
+    public void iCallEndpointToGetCurrentUser() {
+    }
+
+    @Then("I receive http code {int} get get current user")
+    public void iReceiveHttpCodeGetGetCurrentUser(int status) {
+    }
+}
---
+++ src/test/resources/users.feature
@@ -0,0 +1,51 @@
+Feature: the user controller works
+
+  Scenario: Post request to create new user
+    Given I have valid jwt to create new user
+    When I call endpoint to create new user
+    Then I receive http code 201 created new user
+    And I get newly created user
+
+  Scenario: Get request to get all users
+    Given I have valid jwt to get all users
+    When I call endpoint to get all users
+    Then I receive http code 200 for get all users
+    And I get list with all users
+
+  Scenario: Get request to get one user
+    Given I have valid jwt to get one user
+    When I call endpoint to get one user
+    Then I receive http code 200 for get one user
+    And I get one user
+
+  Scenario: Put request to set the password of user
+    Given I have valid jwt to set the password of user
+    When I call endpoint to set password of user
+    Then I receive http code 202 for setting password
+
+  Scenario: Put request to set the role of user
+    Given I have valid jwt to set the role of user
+    When I call endpoint to set role of user
+    Then I receive http code 202 for setting role
+
+  Scenario: Put request to set the status of user
+    Given I have valid jwt to set the status of user
+    When I call endpoint to set status of user
+    Then I receive http code 202 for setting status
+
+  Scenario: Put request to update user
+    Given I have valid jwt to update user
+    When I call endpoint to update user
+    Then I receive http code 200 for updating user
+
+  Scenario: Post request to login
+    Given I have valid credentials to log in
+    When I call endpoint to log into the system
+    Then I receive http code 200 for loggin into the system
+    And I receive valid jwt token
+
+  Scenario: Get request to get current user
+    Given I have valid jwt to get current user
+    When I call endpoint to get current user
+    Then I receive http code 200 get get current user
+    And I receive valid jwt token
